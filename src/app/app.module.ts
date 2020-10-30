5ef2e0027f508108b239ad11e3fda1f4c6b1bf24
devi <devisonallushi@gmail.com>
2020-10-30 15:21:55 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AaComponent } from './aa/aa.component';
+import { DdddddddComponent } from './dddddddd/dddddddd.component';
+import { AlertComponent } from './_components/alert/alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AaComponent,
+    DdddddddComponent,
+    AlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
